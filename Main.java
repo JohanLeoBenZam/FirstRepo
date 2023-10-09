f874b35d1f750057a79c971680a8a24b41de4224
Johan <yojan.benzam@gmail.com>
2023-10-09 09:25:27 +0200

    Main prints Farewell

--- Main.java
+++ Main.java
@@ -1,4 +1,6 @@
 public static void Main(String[] arg){
 	String salutation = "Hola mundo";
 	System.out.println(salutation);
+	String farewell = "Adios con el corazon";
+	System.out.println(farewell);
 }
